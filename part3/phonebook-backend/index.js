f0307562baf7cc90349a159fdbdf8d33dc3a9aef
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-12-22 11:07:40 +0300

    eslint congig added

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-app.use(cors());
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
 // To serve the static file of the frontend
-app.use(express.static('dist'));
-app.use(express.json());
+app.use(express.static('dist'))
+app.use(express.json())
 
 var morgan = require('morgan')
-morgan.token('req-body', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('req-body', function (req, res) { return JSON.stringify(req.body) }) // eslint-disable-line
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 const Contact = require('./models/contact')
@@ -35,30 +35,29 @@ let notes = [
     }
 ]*/
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
-});
+app.get('/', (request, response) => {response.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(contacts => response.json(contacts))
+	Contact.find({}).then(contacts => response.json(contacts))
 })
 
 
 app.get('/info', (request, response) => {
-    const date = new Date();
-    //console.log(Contact)
-    //response.send(`<p>Phonebook has info for {$size "$Contact"} people</p><p>${date}</p>`)
-    Contact.countDocuments({})
-    .then(count => {
-        response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
-    })
-    .catch(error => {
-        response.status(500).json({error: "Error in counting documents"});
-    })
-});
+	const date = new Date()
+	//console.log(Contact)
+	//response.send(`<p>Phonebook has info for {$size "$Contact"} people</p><p>${date}</p>`)
+	Contact.countDocuments({})
+		.then(count => {
+			response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+		})
+		.catch(error => { // eslint-disable-line
+			response.status(500).json({ error: 'Error in counting documents' })
+		})
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
-    /*
+	/*
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
     if(note){
@@ -66,26 +65,26 @@ app.get('/api/persons/:id', (request, response, next) => {
     }
     response.status(404).end()
     */
-   Contact.findById(request.params.id)
-   .then(contact => {
-    if (contact) {
-        response.json(contact)
-    } else {
-        response.status(404).end()
-    }
-   })
-   .catch(error => next(error))
+	Contact.findById(request.params.id)
+		.then(contact => {
+			if (contact) {
+				response.json(contact)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    //const id = Number(request.params.id)
-    //notes = notes.filter(note => note.id !== id)
-    //response.status(204).end()
+	//const id = Number(request.params.id)
+	//notes = notes.filter(note => note.id !== id)
+	//response.status(204).end()
 
-    Contact.findByIdAndDelete(request.params.id)
-   .then(result => {response.status(204).end()})
-   .catch(error => next(error))
+	Contact.findByIdAndDelete(request.params.id)
+		.then(result => {response.status(204).end()}) // eslint-disable-line
+		.catch(error => next(error))
 })
 /*
 const generateId = () => {
@@ -93,8 +92,8 @@ const generateId = () => {
 }*/
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    /*
+	const body = request.body
+	/*
     if(!body.name || !body.number){
         return response.status(400).json({error: 'content missing' })
     }
@@ -104,61 +103,61 @@ app.post('/api/persons', (request, response, next) => {
         })
     }*/
 
-    const person = new Contact({
-        name: body.name,
-        number: body.number,
-        //id: generateId()
-    })
+	const person = new Contact({
+		name: body.name,
+		number: body.number,
+		//id: generateId()
+	})
 
-    //notes = notes.concat(person)
-    //response.json(person)
-    /*const validationError = person.validateSync()
+	//notes = notes.concat(person)
+	//response.json(person)
+	/*const validationError = person.validateSync()
     if(validationError){
         response.status(400).json({ error: validationError.message });
     }*/
-    person.validate()
-    .then(() => person.save())
-    .then(savedPerson => {
-        response.json(savedPerson)
-    })
-    .catch(error => next(error))
+	person.validate()
+		.then(() => person.save())
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    /*const body = request.body
+	/*const body = request.body
     const person = {
         name: body.name,
         number: body.number
     }*/
-    const { name, number } = request.body
+	const { name, number } = request.body
 
-    Contact.findByIdAndUpdate(
-        request.params.id,
-        {name, number},
-        {new: true, runValidators: true, context: 'query'})
-    .then(updatedPerson => {
-        response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+	Contact.findByIdAndUpdate(
+		request.params.id,
+		{ name, number },
+		{ new: true, runValidators: true, context: 'query' })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    if (error.name === 'CastError'){
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError' ){
-        return response.status(400).json({error: error.message})
-    }
-    next(error)
+	console.log(error.message)
+	if (error.name === 'CastError'){
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError' ){
+		return response.status(400).json({ error: error.message })
+	}
+	next(error)
 }
 app.use(errorHandler)
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+	console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,40 +1,41 @@
 const mongoose = require('mongoose')
 
 if(process.argv.length<3){
-    console.log('give password as argument')
-    process.exit(1)
+	console.log('give password as argument')
+	process.exit(1)
 }
 
 const password = process.argv[2]
-const name = process.argv[3];
+/*
+const name = process.argv[3]
 const number = process.argv[4]
-
+*/
 
 const url = `mongodb+srv://natiman58:${password}@cluster0.wkn6iy5.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const phoneBookSchema = new mongoose.Schema({
-    name: String,
-    number: String
+	name: String,
+	number: String
 })
 
 const Phonebook = mongoose.model('Phonebook', phoneBookSchema)
 
-
+/*
 const contact = new Phonebook({
-    name: name,
-    number: number
-})
+	name: name,
+	number: number
+})*/
 
 if (process.argv.length === 3){
-    console.log('phonebook:')
-    Phonebook.find({}).then(result => {
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        //mongoose.connection.close()
-    })
+	console.log('phonebook:')
+	Phonebook.find({}).then(result => {
+		result.forEach(person => {
+			console.log(`${ person.name } ${person.number}`)
+		})
+		//mongoose.connection.close()
+	})
 }
 
 /*
