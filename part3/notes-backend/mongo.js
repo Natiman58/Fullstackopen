eeb2bdf37ee248b55ef2334f104e1558ff4dd338
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-12-22 11:21:49 +0300

    final for part 3

--- part3/notes-backend/mongo.js
+++ part3/notes-backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.legth<3) {
-    console.log('give password as argument')
-    process.exit(1)
+	console.log('give password as argument')
+	process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,34 +13,34 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-  })
-
-  const Note = mongoose.model('Note', noteSchema)
-
-  const note1 = new Note({
-    content: 'HTML is Easy',
-    important: true
-  });
-
-  const note2 = new Note({
-    content: 'Browser can execute only JavaScript',
-    important: false
-  });
-
-  const note3 = new Note({
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  });
-
-  Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    //mongoose.connection.close()
+	content: String,
+	important: Boolean,
 })
-note1.save();
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note1 = new Note({
+	content: 'HTML is Easy',
+	important: true
+})
+
+const note2 = new Note({
+	content: 'Browser can execute only JavaScript',
+	important: false
+})
+
+const note3 = new Note({
+	content: 'GET and POST are the most important methods of HTTP protocol',
+	important: true
+})
+
+Note.find({}).then(result => {
+	result.forEach(note => {
+		console.log(note)
+	})
+	//mongoose.connection.close()
+})
+note1.save()
 note2.save()
 note3.save()
 
